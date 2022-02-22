1469d62020eb69f8fe9725047ace88a731e69591
Jett <premiumconsulting@jettdembin.dev>
2022-02-22 10:54:02 -0500

    added log item section and foods are working for addition and deletion of items after user input weight

--- src/LogItems.js
+++ src/LogItems.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
+import FoodForm from "./FoodForm";
+import FoodList from "./FoodList";
+import useFoodState from "./hooks/useFoodState";
 
 function LogItems(props) {
+  // const initialFoods = JSON.parse(window.localStorage.getItem("foods") || []);
+  const initialFoods = [];
+  const { foods, addFood, removeFood, allowEdit } = useFoodState(initialFoods);
 
   return (
     <Paper>
+      <FoodForm {...props} addFood={addFood} />
+      <FoodList
+        {...props}
+        foods={foods}
+        removeFood={removeFood}
+        allowEdit={allowEdit}
+      />
     </Paper>
   );
 }
